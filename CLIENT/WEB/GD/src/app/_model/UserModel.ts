790a384e9711e16cb311f7cd7dad9c67992b0e47
HieuTran <hieutran.mtg@gmail.com>
2018-09-03 13:39:05 +0700

    aaaaaa

--- CLIENT/WEB/GD/src/app/_model/UserModel.ts
+++
@@ -1,8 +0,0 @@
-export class UserModel{
-    public UserName : string;
-    public Password : string;
-    constructor( user : string , pass : string){
-        this.UserName = user;
-        this.Password = pass;
-    }
-}
